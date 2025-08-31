bd9886a0464e8d3ebda62f46716ab5bf11b62512
nirajsth555 <helloniraj555@gmail.com>
2025-08-31 12:19:17 +0545

    uses recursive text splitter

--- backend/ingestion.py
+++ backend/ingestion.py
@@ -3,7 +3,8 @@ import shutil
 from fastapi import UploadFile
 from utils import load_pdf_text, get_embedding_function
 from langchain.vectorstores import Chroma
-from langchain.text_splitter import CharacterTextSplitter
+# from langchain.text_splitter import CharacterTextSplitter
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 async def ingest_pdf(file: UploadFile):
     os.makedirs(f"uploaded_files", exist_ok=True)
@@ -13,7 +14,13 @@ async def ingest_pdf(file: UploadFile):
         shutil.copyfileobj(file.file, buffer)
 
     text = load_pdf_text(file_path)
-    splitter = CharacterTextSplitter(chunk_size=500, chunk_overlap=50)
+    # splitter = CharacterTextSplitter(chunk_size=500, chunk_overlap=50)
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=500,
+        chunk_overlap=500,
+        length_function=len,
+        separators=["\n\n", "\n", ".", " ", ""]
+    )
     texts = splitter.split_text(text)
 
     embeddings = get_embedding_function()
